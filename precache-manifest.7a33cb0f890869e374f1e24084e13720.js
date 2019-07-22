79783a74fd20141430ad53df45fc5186270089e5
JimmyChang <jimmy.chang@velocitylight.net>
2019-07-22 20:08:02 +0800

    Updates

---
+++ precache-manifest.7a33cb0f890869e374f1e24084e13720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "019d4d76e559ae6d8ce5bdad4b6dacd2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e49cdbc6f85d5a99c1d",
+    "url": "/static/css/main.87916bd9.chunk.css"
+  },
+  {
+    "revision": "b34aa44dad38f1ce2dec",
+    "url": "/static/js/2.38b740f2.chunk.js"
+  },
+  {
+    "revision": "2e49cdbc6f85d5a99c1d",
+    "url": "/static/js/main.e0c0c2a8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
