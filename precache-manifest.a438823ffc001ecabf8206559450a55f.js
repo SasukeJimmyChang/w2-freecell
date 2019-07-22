22f2b37140f7ffdc3649ab0c70ee954a334e57e4
JimmyChang <jimmy.chang@velocitylight.net>
2019-07-22 20:22:20 +0800

    Updates

---
+++ precache-manifest.a438823ffc001ecabf8206559450a55f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d10e1709ade618e4d259ddee2dc92388",
+    "url": "/w2-freecell/index.html"
+  },
+  {
+    "revision": "29a33d710b78011ec51c",
+    "url": "/w2-freecell/static/css/main.87916bd9.chunk.css"
+  },
+  {
+    "revision": "95b163b8297d2637ef2d",
+    "url": "/w2-freecell/static/js/2.6eef9218.chunk.js"
+  },
+  {
+    "revision": "29a33d710b78011ec51c",
+    "url": "/w2-freecell/static/js/main.b662d81b.chunk.js"
+  },
+  {
+    "revision": "4efb9a501f4262585569",
+    "url": "/w2-freecell/static/js/runtime~main.a2f543e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/w2-freecell/static/media/logo.5d5d9eef.svg"
+  }
+]);
